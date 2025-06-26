8e03817c4e7223ade5cff3a93cf4d38f2621a9ce
msakatav <170090251+msakatav@users.noreply.github.com>
2025-06-26 16:21:20 +0900

    06/26 16:29

--- src/playback/play_loop.c
+++ src/playback/play_loop.c
@@ -6,7 +6,7 @@
 /*   By: msakata <msakata@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 09:47:25 by msakata           #+#    #+#             */
-/*   Updated: 2025/06/26 09:48:21 by msakata          ###   ########.fr       */
+/*   Updated: 2025/06/26 16:20:47 by msakata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,7 @@ static void	update_z_angle_and_slider(t_ui *ui)
 
 static void	redraw_play_frame(t_ui *ui)
 {
-	if (ui->image.img)
-		mlx_destroy_image(ui->mlx, ui->image.img);
-	ui->image.img = mlx_new_image(ui->mlx, WIDTH, HEIGHT);
-	ui->image.img_data = mlx_get_data_addr(
-			ui->image.img, &ui->image.bpp,
-			&ui->image.size_lien, &ui->image.endian);
+	ft_memset(ui->image.img_data, 0, WIDTH * HEIGHT * (ui->image.bpp / 8));
 	draw_map(ui, &ui->maps->maps[ui->maps->current]);
 	draw_ui(ui);
 	mlx_put_image_to_window(ui->mlx, ui->win, ui->image.img, 500, 100);
